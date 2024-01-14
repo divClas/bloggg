302c835ac6474c1c6165b995ae02044fea2b25bf
Your Name <144362623+AzatFar@users.noreply.github.com>
2024-01-14 14:34:18 +0300

    dsfdf

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express, { response } from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongosee from "mongoose";
+import { registerValidator } from "./validations/auth.js";
+import { validationResult } from "express-validator";
+import UserModel from "./models/user.js";
 
 mongosee
   .connect(
-    "mongodb+srv://admin:wwwwww@cluster0.quuoow3.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://admin:wwwwww@cluster0.quuoow3.mongodb.net/blog?retryWrites=true&w=majority"
   )
   .then(() => console.log("DB OK"))
   .catch((err) => console.log("DB ERR", err));
@@ -13,8 +17,37 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/auth/register", (req, res) => {
-console.log("sadasds git")
+app.post("/auth/register", registerValidator, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.starus(400).json(errors.array());
+    }
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
+    });
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123"
+    );
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({
+      message: "не удалось зарегистрироваться ",
+    });
+  }
 });
 
 app.listen(4444, (err) => {
